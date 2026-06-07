5381351de1fd863e5e917e2fae5fd2476ceb3f16
Gabriel Rezende <46174954+mourarezendecas@users.noreply.github.com>
2026-06-07 14:00:06 -0300

    Create Motorista.java

---
+++ gemini/Motorista.java
@@ -0,0 +1,17 @@
+package models;
+
+public class Motorista {
+    private String nome;
+
+    public Motorista(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
